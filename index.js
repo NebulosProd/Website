2e1fa69b72ad9e56cbda0f1cac817a4977fa1d5b
FILMA <131806247+FILMA0010@users.noreply.github.com>
2024-01-26 22:39:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views',);
 const { name, description, invite, support, pfp, feature1, feature2, feature3, feature4, port } = require("./config.json")
 const server = https.createServer({
-    cert: fs.readFileSync('fullchain.pem'),
-    key: fs.readFileSync('privkey.pem')
+//    cert: fs.readFileSync('fullchain.pem'),
+//    key: fs.readFileSync('privkey.pem')
   }, app);
 
 server.listen(port, () => {
